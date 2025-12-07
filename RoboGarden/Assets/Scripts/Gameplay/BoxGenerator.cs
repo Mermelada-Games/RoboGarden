5aad473c3a316ccdd467be40aa37eb73a9686786
Sergio <sergiof.sf88@gmail.com>
2025-12-07 12:55:09 +0100

    Box info

--- RoboGarden/Assets/Scripts/Gameplay/BoxGenerator.cs
+++ RoboGarden/Assets/Scripts/Gameplay/BoxGenerator.cs
@@ -5,33 +5,52 @@ public class BoxGenerator : NetworkObject
 {
     [SerializeField] private GameObject BoxPrefab;
     [SerializeField] private Transform[] pathPoints;
+    private readonly string[] arrayEtiquetas = { "Orange", "Blue", "Green" };
 
     public void GenerateBox()
     {
         CancelInvoke(nameof(BroadcastGenerateBox));
-        InvokeRepeating(nameof(BroadcastGenerateBox), 2f, 70f);
+        InvokeRepeating(nameof(BroadcastGenerateBox), 5f, 70f);
     }
 
     private void BroadcastGenerateBox()
     {
-        ReplicationManager.Instance.SendReplication(this.networkId, ReplicationAction.Event, "SpawnBox");
+        string randomEtiqueta = arrayEtiquetas[Random.Range(0, arrayEtiquetas.Length)];
+        int randomDestination = Random.Range(1, 4);
+
+        string payload = $"SpawnBox|{randomEtiqueta}|{randomDestination}";
+        ReplicationManager.Instance.SendReplication(this.networkId, ReplicationAction.Event, payload);
     }
 
     public override void OnReplication(ReplicationAction action, string payload)
     {
-        if (action == ReplicationAction.Event && payload == "SpawnBox")
+        if (action == ReplicationAction.Event)
         {
-            SpawnBox();
+            string[] data = payload.Split('|');
+
+            if (data.Length >= 3 && data[0] == "SpawnBox")
+            {
+                string etiqueta = data[1];
+                int destino = int.Parse(data[2]);
+
+                SpawnBox(etiqueta, destino);
+            }
         }
     }
 
-    private void SpawnBox()
+    private void SpawnBox(string etiqueta, int destino)
     {
         GameObject boxInstance = Instantiate(BoxPrefab, transform.position, Quaternion.identity);
         BoxMovement boxMovement = boxInstance.GetComponent<BoxMovement>();
         if (boxMovement != null)
         {
             boxMovement.SetPath(pathPoints);
+        }
+
+        BoxInteraction boxInteraction = boxInstance.GetComponentInChildren<BoxInteraction>();
+        if (boxInteraction != null)
+        {
+            boxInteraction.InitializeBoxData(etiqueta, destino);
         }
     }
 
